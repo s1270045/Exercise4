116a9fd704a3f5bd8baad6f269e78e503d6e971a
稲垣賢 <inagakiken@inagakikennoMacBook-Pro.local>
2020-08-06 23:06:49 +0900

    User name added

--- HeadsorTails.java
+++ HeadsorTails.java
@@ -7,6 +7,14 @@ public class HeadsorTails{
         
         int heads = 0;
         int tails = 0;
+
+        System.out.println("Who are you?");
+        System.out.print("> ");
+        String name = new java.util.Scanner(System.in).nextLine();
+        System.out.println("Hello, "+ name+ "!");
+
+
+
         System.out.println("Tossing a coin...");
         for(i=0; i<3; i++){
             int a = new Random().nextInt(2);
