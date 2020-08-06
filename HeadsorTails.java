0cf6c0bb3aa683c2426f57c1fd1d53077237d93e
稲垣賢 <inagakiken@inagakikennoMacBook-Pro.local>
2020-08-06 22:56:09 +0900

    HoT: Initial revision

---
+++ HeadsorTails.java
@@ -0,0 +1,27 @@
+// import static java.util.System.*;
+import java.util.Random;
+
+public class HeadsorTails{
+    public static void main(String[] args){
+        int i;
+        
+        int heads = 0;
+        int tails = 0;
+        System.out.println("Tossing a coin...");
+        for(i=0; i<3; i++){
+            int a = new Random().nextInt(2);
+            if(a==0){
+                System.out.println("Round "+i+ ": "+"Heads");
+                heads++;
+            }
+            else if(a==1){
+                System.out.println("Round "+i+": "+"Tails");
+                tails++;
+            }
+        }
+        System.out.print("Heads: "+heads);
+        System.out.println(", Tails: "+tails);
+
+        
+    }
+}
